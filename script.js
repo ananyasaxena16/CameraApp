de88bb2ea83f71388708551b96cd52931e3e2da2
Ananya Saxena <80524036+ananyasaxena16@users.noreply.github.com>
2021-05-29 18:44:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,103 @@
-let constraints = { video: true, audio: true };
-
-let videoPlayer = document.querySelector("video");
-let vidRecordBtn = document.querySelector("#record-video");
-
+let constraints = {video:true, audio:true};
+let videoPlayer = document.querySelector('video');
+let vidRecordBtn = document.querySelector('#record-video');
+let captureBtn = document.querySelector('#click-picture');
 let mediaRecorder;
-let chunks = [];
 let recordState = false;
+let chunks = [];
+let filter = '';
 
-// C2.2
+let allFilters = document.querySelectorAll('.filter');
+for(let i=0;i<allFilters.length;i++)
+{
+    allFilters[i].addEventListener('click',function(e){
+        filter = e.currentTarget.style.backgroundColor;
+        removeFilter();
+        addFilterToScreen(filter);
+    })
+}
 
+function addFilterToScreen(filterColor)
+{
+    let filter = document.createElement('div');
+    filter.classList.add('on-screen-filter');
+    filter.style.height='100vh';
+    filter.style.width='100vw';
+    filter.style.backgroundColor=`${filterColor}`;
+    filter.style.position = 'fixed';
+    filter.style.top= '0px';
+    document.querySelector('body').appendChild(filter);
+}
+function removeFilter(){
+    let el = document.querySelector('.on-screen-filter');
+    if(el)
+    {
+        el.remove();
+    }
+}
 
-vidRecordBtn.addEventListener("click", function () {
-  if (!recordState) {
-    recordState = true;
-    mediaRecorder.start();
-    vidRecordBtn.innerText = "Recording...";
-  } else {
-    recordState = false;
-    mediaRecorder.stop();
-    vidRecordBtn.innerText = "Record";
+vidRecordBtn.addEventListener("click",function(){
+  if(mediaRecorder!=undefined)
+  {
+      let innerDiv = vidRecordBtn.querySelector('#record-div');
+  if(recordState==false)
+  {
+      recordState=true;
+      innerDiv.classList.add('recording-animation');
+      mediaRecorder.start();
   }
-});
-
-navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
-  videoPlayer.srcObject = mediaStream;
-
-  mediaRecorder = new MediaRecorder(mediaStream);
-
-  mediaRecorder.ondataavailable = function (e) {
-    chunks.push(e.data);
-  };
-
-  mediaRecorder.onstop = function () {
-    let blob = new Blob(chunks, { type: "video/mp4" });
-    chunks = [];
-
-    var blobUrl = URL.createObjectURL(blob);
-
-    var link = document.createElement("a");
+  else{
+      recordState=false;
+      innerDiv.classList.remove('recording-animation');
+      mediaRecorder.stop();
+      
+  }
+}
+})
+navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+  videoPlayer.srcObject=mediaStream;
+mediaRecorder = new MediaRecorder(mediaStream);
+mediaRecorder.ondataavailable = function(e){
+    chunks.push(e.data)
+}
+mediaRecorder.onstop=function()
+{
+    let blob = new Blob(chunks,{type:'video/mp4'});
+    chunks =[];
+    let blobUrl = URL.createObjectURL(blob);
+    var link = document.createElement('a');
     link.href = blobUrl;
-    link.download = `${Date.now()}.mp4`;
+    link.download='video.mp4';
     link.click();
     link.remove();
-  };
-});
+}
+
+
+}).catch(function(err){
+  console.log(err);
+})
+captureBtn.addEventListener('click',function()
+{
+  let innerDiv = captureBtn.querySelector('#click-div');
+  innerDiv.classList.add('capture-animation');
+  console.log(('clicked'));
+  capture();
+
+  setTimeout(function(){
+      innerDiv.classList.remove('capture-animation');
+  },1000);
+})
+function capture()
+{
+    let c = document.createElement('canvas');
+    c.width = videoPlayer.videoWidth;
+    c.height = videoPlayer.videoHeight;
+    let tool = c.getContext('2d');
+    tool.drawImage(videoPlayer,0,0);
+    let link = document.createElement('a');
+    link.download = 'image.png';
+    link.href = c.toDataURL();
+    link.click();
+    link.remove();
+    c.remove();
+}
